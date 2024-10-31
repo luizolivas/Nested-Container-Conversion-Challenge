bd2b30cc4bf0ed67e4dac1a971a08d60b27bbf78
luizolivas <luizolivas@gmail.com>
2024-10-31 14:22:14 -0300

    Add method to flatten nested containers

--- ArrayContainerTest/Program.cs
+++ ArrayContainerTest/Program.cs
@@ -1,12 +1,14 @@
-﻿
-Dictionary<string, Object> container =
+﻿Dictionary<string, Object> container =
     new Dictionary<string, Object>();
+
+
+
 
 
 container.Add("one", new Dictionary<string, Object>
 {
     { "two", 3 },
-    { "four", new List<int> [5,6,7] },
+    { "four", new List<Object> {5,6,7} },
 });
 container.Add("eight", new Dictionary<string, Object>
 {
@@ -18,10 +20,36 @@ container.Add("eight", new Dictionary<string, Object>
 });
 
 
+Dictionary<string, Object> containerFlat =
+    new Dictionary<string, Object>();
 
-
-foreach (KeyValuePair<string, Object> kvp in container)
+void printContainer(Dictionary<string, Object> container, string parentKey = "")
 {
-    Console.WriteLine("Key = {0}, Value = {1}",
-        kvp.Key, kvp.Value);
+    foreach (var kvp in container)
+    {
+        string currentKey = string.IsNullOrEmpty(parentKey) ? kvp.Key : $"{parentKey}/{kvp.Key}";
+
+        if (kvp.Value is Dictionary<string, Object> next)
+        {
+            printContainer(next, currentKey);
+        }
+        else if (kvp.Value is List<Object> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                containerFlat[$"{currentKey}/{i}"] = list[i];
+            }
+        }
+        else
+        {
+            containerFlat.Add(currentKey, (kvp.Value));
+        }
+    }
+}
+
+printContainer(container);
+
+foreach (var kvp in containerFlat)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
 }
